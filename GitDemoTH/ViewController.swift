8027d34dce169cb7dbeab8d9632a77d19c1bc6aa
Joel Martin <joelbenjaminmartin@hotmail.com>
2016-04-10 21:19:09 -0400

    Initial Commit

---
+++ GitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoTH
+//
+//  Created by Joel Martin on 2016-04-10.
+//  Copyright © 2016 Joel Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
